b7544a6fcc960ccc8f0a3bf8c5349349a4b52a0f
mehmetkudsiodaman <104054089+mehmetkudsiodaman@users.noreply.github.com>
2022-05-17 19:15:57 +0300

    Variables lesson

---
+++ dotnet/csharp101/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace csharp101
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString;
+            inputString = Console.ReadLine();
+
+            // Print a string literal saying "Hello, World." to stdout using cout.
+            Console.WriteLine("Hello, World.");
+
+            // TODO: Write a line of code here that prints the contents of input_string to stdout.
+            Console.WriteLine(inputString);
+        }
+    }
+}
